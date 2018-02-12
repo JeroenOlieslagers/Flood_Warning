39def2aaed14f2443736e69b85cac5255f21a2df
joliesla <34600363+joliesla@users.noreply.github.com>
2018-02-12 11:53:50 +0000

    Start Task2E

---
+++ Task2E.py
@@ -0,0 +1,6 @@
+def run():
+    """Requirement for Task 2E"""
+    
+if __name__ == "__main__":
+    print("*** Task 2E: CUED Part IA Flood Warning System ***")
+    run()
---
+++ floodsystem/plot.py
@@ -0,0 +1,19 @@
+from floodsystem.datafetcher import fetch_measure_levels
+import matplotlib.pyplot as plt
+
+def plot_water_levels(station, dates, levels):
+    """ A function that displays a plot (using Matplotlib) of the water level
+    data against time for a station, and includes on the plot lines for the
+    typical low and high levels """
+    # Plot
+    plt.plot(dates, levels)
+
+    # Add axis labels, rotate date labels and add plot title
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45);
+    plt.title(station.name)
+
+    # Display plot
+    plt.tight_layout()  # This makes sure plot does not cut off date labels
+    plt.show()
