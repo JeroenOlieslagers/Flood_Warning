98e72b32a70c505335d352c8a678ebf4417a22fd
joliesla <34600363+joliesla@users.noreply.github.com>
2018-02-13 00:20:21 +0000

    Working function but in wrong file (will fix tmr)

--- Task2E.py
+++ Task2E.py
@@ -14,14 +14,16 @@ def run():
     data = build_station_list()
     ls = []
     dt = 5
-    i = 1
-    limit = 25
-    days = mdates.DateLocator()
-    daysFmt = mdates.DateFormatter('%D')
-    fig, ax = plt.subplots()
+    i = 0
+    j = 0
+    limit = 49
+    
+    #X-axis every day
+    days = mdates.DayLocator()
+    daysFmt = mdates.DateFormatter('%d-%m')
     
     #Get list of measuring_id's and rel water level
-    for station in data[:25]:
+    for station in data[:49]:
         ls.append((station.measure_id, station.typical_range[1], station.name))#station.relative_water_level())
     
     #Create list of measuring_id's sorted by water level
@@ -29,9 +31,12 @@ def run():
     
     #Set orientation of graphs
     sq = sqrt(len(ls))
-    if sq.is_integer():
-        row = sq
-        col = sq
+    if len(ls) > limit:
+        row = 1
+        col = 1
+    elif sq.is_integer():
+        row = int(sq)
+        col = int(sq)
     else:
         if sq - int(sq) < 0.5:
             row = int(sq)
@@ -39,25 +44,33 @@ def run():
         else:
             row = (int(sq)+1)
             col = (int(sq)+1)
-        
+    
     #Loop over all stations
+    fig, axarr = plt.subplots(row, col)
     for item in ls:
         a = fetch_measure_levels(item[0], dt=datetime.timedelta(days=dt))
         #On different graphs if no. of stations less than limit
         if len(ls) <= limit:
-            ax.subplot(row, col, i)
-            plt.plot(a[0],a[1])
-            plt.title(item[2])
-            i += 1
+            #Set details for each subplot
+            axarr[i, j].plot(a[0],a[1])
+            axarr[i, j].xaxis.set_major_locator(days)
+            axarr[i, j].xaxis.set_major_formatter(daysFmt)
+            axarr[i, j].set_title(item[2])
+            #Algorithm to move from top to bottom, left to right
+            if i < row-1:
+                i += 1
+            elif i == row-1:
+                j += 1
+                i = 0
             plt.subplots_adjust(bottom=0.02, right=0.98, left=0.02, top=0.98)
+            
         #On same graph if no. of stations greater than limit            
         else:
-            ax.plt.plot(a[0],a[1], label=item[2])
+            plt.plot(a[0],a[1], label=item[2])
             plt.subplots_adjust(bottom=0.02, right=0.9, left=0.02, top=0.98)
-            
-     
-    plt.legend(bbox_to_anchor=(1.005, 1), loc=2, borderaxespad=0.)    
-
+            plt.legend(bbox_to_anchor=(1.005, 1), loc=2, borderaxespad=0.) 
+    
+    
     
 if __name__ == "__main__":
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
